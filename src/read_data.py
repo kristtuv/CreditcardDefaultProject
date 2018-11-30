a1ef279be1111b6b88341b252d50e9cc883f0887
kristtuv <ktuv@hotmail.com>
2018-11-30 15:33:47 +0100

    Dette er en commit

--- src/read_data.py
+++ src/read_data.py
@@ -1,5 +1,30 @@
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from typing import Tuple
 
-df = pd.read_excel('../credit_data/default of credit card clients.xls')
+def get_data(normalized: bool=True, 
+             standardized: bool=False) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Fetches the data for credit card defaults. Can also normalize or standardize
+    the data before returning if argumetens are given
+    """
+    df = pd.read_excel('../credit_data/default of credit card clients.xls')
+    X = np.array(df.iloc[1:,:-1])
+    Y = np.array(df.iloc[1:, -1]).reshape(-1, 1)
+    if normalized:
+        scaler = MinMaxScaler(feature_range=(0,1))
+        scaledX = scaler.fit_transform(X)
+        return scaledX, Y
 
-print(df.head(4))
+    elif standardized:
+        scaler = StandardScaler()
+        scaledX = scaler.fit_transform(X)
+        return scaledX, Y
+
+    else:
+        return X, Y
+    
+if __name__=='__main__':
+    get_data(normalized=False, standardized=True)
